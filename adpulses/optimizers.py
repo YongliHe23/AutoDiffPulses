44c8bd94d6f1630c2e153f7135a176137cf0cb79
YongliHe23 <yonglihe@umich.edu>
2023-11-13 14:52:58 -0500

    new

--- adpulses/optimizers.py
+++ adpulses/optimizers.py
@@ -367,9 +367,9 @@ def arctanSGD(
     # enforce contiguousness of optimization variables, o.w. LBFGS may fail
     tρ, θ, tsl = tρ.contiguous(), θ.contiguous(), tsl.contiguous()
 
-    opt_rf = optim.SGD([tρ, θ], lr=0.01,momentum=0.9,nesterov=True)
+    opt_rf = optim.SGD([tρ, θ], lr=0.1)
 
-    opt_sl = optim.SGD([tsl], lr=0.01,momentum=0.9,nesterov=True)
+    opt_sl = optim.SGD([tsl], lr=0.1)
 
     tρ.requires_grad = θ.requires_grad = tsl.requires_grad = True
 
